7d88d9fcfe013f3d062f6dc36311b585304f3b9e
Hai Do Minh <hai.do@ntq-solution.com.vn>
2019-08-26 17:26:38 +0700

    update-reducer

--- reducers/reducers.js
+++ reducers/reducers.js
@@ -1,19 +1,5 @@
 import { combineReducers } from 'redux';
-
-function counter(state, action) {
-    if (typeof state == 'undefined') {
-        return 0;
-    }
-
-    switch(action.type) {
-        case 'INCREMENT':
-            return state + 1;
-        case 'DECREMENT':
-            return state - 1;
-        default:
-            return state;
-    }
-}
+import counter from './countReducer';
 
 const countApp = combineReducers({count: counter});
 
